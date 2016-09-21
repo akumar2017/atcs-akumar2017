f8c0d83f8f08e588a091b89bdfd2e456be38f1db
akumar2017 <akumar2017@pingry.org>
2016-09-21 08:37:44 -0400

    Create PairOfStrings.java

---
+++ GitProject/PairOfStrings.java
@@ -0,0 +1,16 @@
+public class PairOfStrings
+{ 
+	private String string1; 
+	private String string2;
+
+	public PairOfStrings(String s1, String s2)
+	{
+		s1 = string1;
+		s2 = string2;
+	}
+
+	public String toString()
+	{
+		return string1 + " " + string2;
+	}
+}
